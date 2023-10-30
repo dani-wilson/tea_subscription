8e5e16329f1ac6a2424139899fc1039203ac00aa
Dani Wilson <danirae.powell@gmail.com>
2023-10-30 15:01:06 -0600

    chore: add migrations for data tables

---
+++ db/migrate/20231030203525_add_fk_to_subscriptions.rb
@@ -0,0 +1,6 @@
+class AddFkToSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :subscriptions, :customer, null: false, foreign_key: true
+    add_reference :subscriptions, :tea, null: false, foreign_key: true
+  end
+end
