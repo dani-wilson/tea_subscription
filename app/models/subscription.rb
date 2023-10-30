2838d29549f225b43269e5884b697a2e93b6a0a9
Dani Wilson <danirae.powell@gmail.com>
2023-10-30 15:00:45 -0600

    feat: add subscription model and validations

---
+++ app/models/subscription.rb
@@ -0,0 +1,9 @@
+class Subscription < ApplicationRecord
+  validates_presence_of :title,
+                        :price,
+                        :status,
+                        :frequency
+
+  belongs_to :tea
+  belongs_to :customer
+end
