4bf0bcd06805d43560958bb09bad1980b3f0521a
Dani Wilson <danirae.powell@gmail.com>
2023-10-30 19:27:44 -0600

    chore: add faker factories

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
-  factory :subscriptions do
+  factory :subscription do
     title { "#{Faker::Subscription.subscription_term} #{Faker::Tea.variety}" }
     price { Faker::Number.decimal(l_digits: 2, r_digits: 2)}
     status { Faker::Number.between(from: 0, to: 1)}
     frequency { Faker::Subscription.subscription_term }
+    tea_id { Faker::Number.between(from: 1, to: 20)}
+    customer_id { Faker::Number.between(from: 1, to: 5)}
   end
 end
