21d485111212b6e708aac5810bfbfd8e42de8d9c
Dani Wilson <danirae.powell@gmail.com>
2023-10-31 13:31:01 -0600

    feat: add destroy action for controller

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -5,6 +5,13 @@ class Api::V0::SubscriptionsController < ApplicationController
     render json: { message: "#{Subscription.last.title} added."}, status: 201
   end
 
+  def destroy
+    subscription = Subscription.find(params[:id])
+    subscription_title = subscription.title
+    subscription.destroy!
+    render json: { message: "#{subscription_title} deleted." }
+  end
+
   private
   def subscription_params
     params.permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
