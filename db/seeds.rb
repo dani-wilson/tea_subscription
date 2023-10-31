c17bd48bb5b51d2cb9ae8f88b580dc64457da56c
Dani Wilson <danirae.powell@gmail.com>
2023-10-30 19:27:12 -0600

    test: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,19 @@ Tea.destroy_all
 Customer.destroy_all
 Subscription.destroy_all
 
+require "faker"
+require "factory_bot_rails"
+
+5.times do
+  FactoryBot.create(:customer)
+end
+
+20.times do
+  FactoryBot.create(:tea)
+end
+
+10.times do
+  FactoryBot.create(:subscription, customer: Customer.all.sample, tea: Tea.all.sample)
+end
+
+
