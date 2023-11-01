ab7ff88771efc36d44d4f782daa0ca4ebd1cef15
Dani Wilson <danirae.powell@gmail.com>
2023-11-01 10:41:10 -0600

    fix: add routes for customer subscription index

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
       resources :subscriptions, only: [:create, :destroy, :index]
     end
   end
+
+  namespace :api do
+    namespace :v0 do
+      resources :customers do
+        resources :subscriptions, only: [:index]
+      end
+    end
+  end
 end
